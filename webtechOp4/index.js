b66cf9585c5611617a5881e68d8f1f3f310426f2
RicardoNijsink <ricardonijsink@gmail.com>
2015-10-20 11:05:55 +0200

    Pagina's bijgewerkt

--- webtechOp4/index.js
+++ webtechOp4/index.js
@@ -1,30 +1,12 @@
 $(document).ready(function () {
 
+    $('.buttonNotflix').click(function(){
+      window.location.href='films.html';
+    });
 
-$('#contact-form').validate({
-	console.log("called")
-    rules: {
-        name: {
-            minlength: 2,
-            required: true
-        },
-        email: {
-            required: true,
-            email: true
-        },
-        message: {
-            minlength: 2,
-            required: true
-        }
-    },
-    highlight: function (element) {
-        $(element).closest('.control-group').removeClass('success').addClass('error');
-    },
-    success: function (element) {
-        element.text('OK!').addClass('valid')
-            .closest('.control-group').removeClass('error').addClass('success');
-    }
-});
-console.log("called2")
+    $('#buttonLogin').click(function(){
+      $("#loginForm").hide();
+      $("#loggedInDropdown").show();
+    });
 });
 
